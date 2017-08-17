791222111e597df5b0c88e98d3eda7041c3b8016
ZYZ <569407547@qq.com>
2017-08-17 09:04:38 +0800

    营运管理员，客户信息管理表格初绘制

--- src/components/operationAdministrator/PersonnelInfo.vue
+++ src/components/operationAdministrator/PersonnelInfo.vue
@@ -1,3 +1,83 @@
 <template>
-  <div>客户信息管理</div>
+  <div>
+    <div>
+      <div style="float: right">
+        <el-input type="text" placeholder="请输入要搜索的内容" @input="onQuickFilterChanged"></el-input>
+      </div>
+      <div>
+        <el-button>添加</el-button>
+      </div>
+    </div>
+    <div style="clear: both;"></div>
+    <div>
+      <ag-grid-vue style="width: 100%;height: 350px" class="ag-blue"
+                   :gridOptions="gridOptions"
+                   :suppressMovableColumns="true"
+                   :enableColResize="true"
+                   :enableSorting="true"
+                   :enableFilter="true"
+                   :groupHeaders="true"
+      ></ag-grid-vue>
+    </div>
+  </div>
 </template>
+
+<script>
+  import {AgGridVue} from 'ag-grid-vue'
+  import testJson from '../../../static/test/testJSON.js'
+  export default {
+    data () {
+      return {
+        gridOptions: {
+          columnDefs: null,
+          rowData: null
+        }
+      }
+    },
+    components: {
+      'ag-grid-vue': AgGridVue
+    },
+    methods: {
+      createRowData () {
+        this.gridOptions.rowData = testJson.personnelInfo.list
+      },
+      createColumnDefs () {
+        this.gridOptions.columnDefs = [
+          {
+            headerName: '客户企业ID', width: 150, field: 'clientCompId', filter: 'text'
+          },
+          {
+            headerName: '客户企业名称', width: 150, field: 'clientCompNam', filter: 'text'
+          },
+          {
+            headerName: '联系人姓名', width: 150, field: 'nam', filter: 'text'
+          },
+          {
+            headerName: '联系电话', width: 150, field: 'tel', filter: 'text'
+          },
+          {
+            headerName: '企业详细地址', width: 150, field: 'compAdr', filter: 'text'
+          },
+          {
+            headerName: '所属片区', width: 150, field: 'area', filter: 'text'
+          },
+          {
+            headerName: '业务员ID', width: 150, field: 'salesmanId', filter: 'text'
+          },
+          {
+            headerName: '是否三方', width: 150, field: 'isTril', filter: 'text'
+          }
+
+        ]
+      },
+      onQuickFilterChanged (input) {
+        this.gridOptions.api.setQuickFilter(input)
+      }
+    },
+    beforeMount () {
+      this.createRowData()
+      this.createColumnDefs()
+      console.log(this.gridOptions)
+    }
+  }
+</script>
---
+++ static/test/testJSON.js
@@ -0,0 +1,26 @@
+export default {
+  "personnelInfo": {
+    "list": [
+      {
+        "clientCompId":"1111",
+        "clientCompNam": "test1",
+        "nam": "test1",
+        "tel": "11111",
+        "compAdr": "1111",
+        "area": "1111",
+        "salesmanId": "1111",
+        "isTril": "1",
+      },
+      {
+        "clientCompId":"222",
+        "clientCompNam": "test2",
+        "nam": "test2",
+        "tel": "2222",
+        "compAdr": "222",
+        "area": "22",
+        "salesmanId": "222",
+        "isTril": "1",
+      }
+    ]
+  }
+}
