24fdb2b958a5130683087ed002e818fe181677ff
ZYZ <569407547@qq.com>
2017-08-17 11:41:44 +0800

    表格调整（未完成）

--- src/components/operationAdministrator/PersonnelInfo.vue
+++ src/components/operationAdministrator/PersonnelInfo.vue
@@ -5,14 +5,20 @@
         <el-input type="text" placeholder="请输入要搜索的内容" @input="onQuickFilterChanged"></el-input>
       </div>
       <div>
-        <el-button>添加</el-button>
+        <el-button @click="test">添加</el-button>
+        <el-button @click="colVisible = true">设置</el-button>
       </div>
+
+      <el-dialog title="个人中心:" :visible.sync="colVisible" size="tiny">
+        <template v-for="(collist,i) in gridOptions.columnDefs">
+          <el-checkbox v-model="gridOptions.columnDefs[i].hide">{{collist.headerName}}</el-checkbox>
+        </template>
+      </el-dialog>
     </div>
     <div style="clear: both;"></div>
     <div>
       <ag-grid-vue style="width: 100%;height: 350px" class="ag-blue"
                    :gridOptions="gridOptions"
-                   :suppressMovableColumns="true"
                    :enableColResize="true"
                    :enableSorting="true"
                    :enableFilter="true"
@@ -25,13 +31,15 @@
 <script>
   import {AgGridVue} from 'ag-grid-vue'
   import testJson from '../../../static/test/testJSON.js'
+
   export default {
     data () {
       return {
         gridOptions: {
-          columnDefs: null,
-          rowData: null
-        }
+          rowData: null,
+          columnDefs: null
+        },
+        colVisible: false
       }
     },
     components: {
@@ -44,34 +52,46 @@
       createColumnDefs () {
         this.gridOptions.columnDefs = [
           {
-            headerName: '客户企业ID', width: 150, field: 'clientCompId', filter: 'text'
+            headerName: '客户企业ID', width: 150, field: 'clientCompId', filter: 'text', hide: false
           },
           {
-            headerName: '客户企业名称', width: 150, field: 'clientCompNam', filter: 'text'
+            headerName: '客户企业名称', width: 150, field: 'clientCompNam', filter: 'text', hide: false
           },
           {
-            headerName: '联系人姓名', width: 150, field: 'nam', filter: 'text'
+            headerName: '联系人姓名', width: 150, field: 'nam', filter: 'text', hide: false
           },
           {
-            headerName: '联系电话', width: 150, field: 'tel', filter: 'text'
+            headerName: '联系电话', width: 150, field: 'tel', filter: 'text', hide: false
           },
           {
-            headerName: '企业详细地址', width: 150, field: 'compAdr', filter: 'text'
+            headerName: '企业详细地址', width: 150, field: 'compAdr', filter: 'text', hide: false
           },
           {
-            headerName: '所属片区', width: 150, field: 'area', filter: 'text'
+            headerName: '所属片区', width: 150, field: 'area', filter: 'text', hide: false
           },
           {
-            headerName: '业务员ID', width: 150, field: 'salesmanId', filter: 'text'
+            headerName: '业务员ID', width: 150, field: 'salesmanId', filter: 'text', hide: false
           },
           {
-            headerName: '是否三方', width: 150, field: 'isTril', filter: 'text'
+            headerName: '是否三方', width: 150, field: 'isTril', filter: 'text', hide: false
           }
-
         ]
       },
       onQuickFilterChanged (input) {
         this.gridOptions.api.setQuickFilter(input)
+      },
+      test () {
+//        console.log(this.gridOptions.columnDefs[0].hide)
+//        console.log(this.gridOptions.columnApi.getAllDisplayedColumns())
+        console.log(this.gridOptions.columnApi.getColumnState())
+      },
+      setting () {
+
+      }
+    },
+    computed: {
+      getcol () {
+
       }
     },
     beforeMount () {
