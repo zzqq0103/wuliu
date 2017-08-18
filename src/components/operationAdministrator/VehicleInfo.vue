08474990eba886497a2dbeebbe17058604208ae3
lilili <939033853@qq.com>
2017-08-18 15:22:51 +0800

    更改车辆信息弹窗

--- src/components/operationAdministrator/VehicleInfo.vue
+++ src/components/operationAdministrator/VehicleInfo.vue
@@ -166,15 +166,16 @@
             this.params.context.componentParent.vehicleDelVisable = true
           },
           vehicleEdit () {
-            var vehicleform = this.params.context.componentParent.vehicleForm
+            /* var vehicleform = this.params.context.componentParent.vehicleForm
             vehicleform.licePlateNum = testJson.vehicleInfo.list[this.params.node.rowIndex].licePlateNum
             vehicleform.driverName = testJson.vehicleInfo.list[this.params.node.rowIndex].driverName
             vehicleform.tel = testJson.vehicleInfo.list[this.params.node.rowIndex].tel
             vehicleform.capacity = testJson.vehicleInfo.list[this.params.node.rowIndex].capacity
             vehicleform.carType = testJson.vehicleInfo.list[this.params.node.rowIndex].carType
             vehicleform.pickUpArea = testJson.vehicleInfo.list[this.params.node.rowIndex].pickUpArea
-            vehicleform.carPosition = testJson.vehicleInfo.list[this.params.node.rowIndex].carPosition
+            vehicleform.carPosition = testJson.vehicleInfo.list[this.params.node.rowIndex].carPosition */
             this.params.context.componentParent.vehicleVisable = true
+            this.params.context.componentParent.vehicleForm = this.params.data
           }
         }
       }
