a839b9b2f497bcc87f1491f6aeb340e362876fce
yty <yantingyong86@163.com>
2017-08-22 10:05:49 +0800

    外包信息

--- src/components/operationAdministrator/Enterprise.vue
+++ src/components/operationAdministrator/Enterprise.vue
@@ -10,7 +10,7 @@
         </div>
         <div>
           <!--<el-button @click="vehicleVisable = true">添加</el-button>-->
-          <!-- <el-button @click="vehicleAdd">添加</el-button> -->
+          <el-button @click="vehicleAdd">添加</el-button>
           <!-- <el-button @click="setting">设置</el-button> -->
         </div>
       </p>
@@ -32,31 +32,27 @@
 
     <el-dialog title="车辆信息:" :visible.sync="vehicleVisable">
         <el-form :model="vehicleForm" :rules="rules" ref="vehicleForm">
-          <el-form-item label="车牌号码:" :label-width="formLabelWidth">
+          <el-form-item label="公司名称:" :label-width="formLabelWidth">
             <el-input v-model="vehicleForm.licePlateNum"></el-input>
           </el-form-item>
-          <el-form-item label="司机姓名:" :label-width="formLabelWidth">
+          <el-form-item label="中转起始点:" :label-width="formLabelWidth">
             <el-input v-model="vehicleForm.driverName"></el-input>
           </el-form-item>
-          <el-form-item label="联系电话:" :label-width="formLabelWidth">
+          <el-form-item label="中转目的地:" :label-width="formLabelWidth">
             <el-input v-model="vehicleForm.tel"></el-input>
           </el-form-item>
-          <el-form-item label="车容量:" :label-width="formLabelWidth">
+          <el-form-item label="联系人:" :label-width="formLabelWidth">
             <el-input v-model="vehicleForm.capacity"></el-input>
           </el-form-item>
-          <el-form-item label="车辆类型:" :label-width="formLabelWidth">
+          <el-form-item label="联系方式:" :label-width="formLabelWidth">
             <el-input v-model="vehicleForm.carType"></el-input>
           </el-form-item>
-          <el-form-item label="车辆位置:" :label-width="formLabelWidth">
+          <el-form-item label="合同号:" :label-width="formLabelWidth">
             <el-input v-model="vehicleForm.carPosition"></el-input>
           </el-form-item>
-          <el-form-item label="车辆接送区域:" label-width="150px">
-            <el-select v-model="vehicleForm.pickUpArea" placeholder="请选择" style="width:100%" >
-                <el-option key="ct" label="长途" value="ct"></el-option>
-                <el-option key="dt" label="短途" value="dt"></el-option>
-                <el-option key="ctdt" label="长途与短途" value="ctdt"></el-option>
-            </el-select>
-          </el-form-item>
+          <el-form-item label="合同价格:" :label-width="formLabelWidth">
+            <el-input v-model="vehicleForm.carPositionjiage"></el-input>
+          </el-form-item>          
         </el-form>
         <div slot="footer" class="dialog-footer">
           <el-button @click="vehicleVisable = false">取 消</el-button>
@@ -116,40 +112,25 @@
           rowData: null,
           columnDefs: [
             {
-              headerName: '车牌号码', width: 150, field: 'licePlateNum', filter: 'text', hide: false
+              headerName: '公司名称', width: 150, field: 'licePlateNum', filter: 'text', hide: false
             },
             {
-              headerName: '司机姓名', width: 150, field: 'driverName', filter: 'text', hide: false
+              headerName: '中转起始点', width: 150, field: 'driverName', filter: 'text', hide: false
             },
             {
-              headerName: '联系电话', width: 150, field: 'tel', filter: 'text', hide: false
+              headerName: '中转目的地', width: 150, field: 'tel', filter: 'text', hide: false
             },
             {
-              headerName: '合同号', width: 150, field: 'contractID', filter: 'text', hide: false
+              headerName: '联系人', width: 150, field: 'contractID', filter: 'text', hide: false
             },
             {
-              headerName: '合同价格', width: 150, field: 'contractPrice', filter: 'text', hide: false
+              headerName: '联系方式', width: 150, field: 'contractPrice', filter: 'text', hide: false
             },
             {
-              headerName: '车容量', width: 150, field: 'capacity', filter: 'text', hide: false
+              headerName: '合同号', width: 150, field: 'capacity', filter: 'text', hide: false
             },
             {
-              headerName: '吨位', width: 150, field: 'tonnage', filter: 'text', hide: false
-            },
-            {
-              headerName: '车辆类型', width: 150, field: 'carType', filter: 'text', hide: false
-            },
-            {
-              headerName: '车辆接送区域', width: 150, field: 'pickUpArea', filter: 'text', hide: false
-            },
-            {
-              headerName: '司机状态', width: 150, field: 'receState', filter: 'text', hide: false
-            },
-            {
-              headerName: '车辆状态', width: 150, field: 'carState', filter: 'text', hide: false
-            },
-            {
-              headerName: '车辆位置', width: 150, field: 'carPosition', filter: 'text', hide: false
+              headerName: '合同价格', width: 150, field: 'tonnage', filter: 'text', hide: false
             },
             {
               headerName: '操作', field: 'value', width: 150, cellRendererFramework: 'operateComponent', hide: false
