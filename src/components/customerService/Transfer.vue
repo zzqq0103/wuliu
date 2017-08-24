c962269392700cf89a14d97c1a1126d3a387dbbf
yty <yantingyong86@163.com>
2017-08-24 17:27:47 +0800

    no message

--- src/components/customerService/Transfer.vue
+++ src/components/customerService/Transfer.vue
@@ -1,3 +1,154 @@
 <template>
-    <div>回单管理</div>
+    <div>
+        <div>
+            <h2 style="text-align:center">回单管理</h2>
+            <p style="margin-top:1%;float:left" class="appointment">
+                <el-h5 class="appointmenttime">开单时间</el-h5>
+                <el-input type="date" class="appointmenttimes" placeholder="开始时间"></el-input>
+                <el-h5 class="appointmenttimes">到</el-h5>
+                <el-input type="date" class="appointmenttimes" placeholder="结束时间"></el-input>
+                <el-h5 class="appointmenttimes">预约单号：</el-h5>
+                <el-input class="appointmenttimeman"></el-input>
+                <el-h5 class="appointmenttimes">回单状态：</el-h5>
+                <el-select  placeholder="选择" class="appointmentoption" style='width:100px'>
+                    <el-option key="yes" label="待生成" value="yes"></el-option>
+                    <el-option key="no" label="已生成" value="no"></el-option>
+                </el-select>
+                <el-h5 class="appointmenttimes">回单押款状态：</el-h5>
+                <el-select  placeholder="选择" class="appointmentoption" style='width:100px'>
+                    <el-option key="yes" label="已付款" value="yes"></el-option>
+                    <el-option key="no" label="未付款" value="no"></el-option>
+                </el-select>
+            </p>
+            <p style="margin-top:1%;float:right;margin-right:5%">
+                <el-button>查找</el-button>
+                <el-button>导出</el-button>
+            </p>
+        </div>
+        <div style="clear: both;">
+        </div>
+        <div style="margin-top:2%">
+            <ag-grid-vue style="width: 100%;height: 350px" class="ag-blue" :gridOptions="gridOptions" :suppressMovableColumns="true" :enableColResize="true" :enableSorting="true" :enableFilter="true" :groupHeaders="true" :rowHeight="40" :headerHeight="30"></ag-grid-vue>
+        </div>
+    </div>
 </template>
+<script>
+  import {AgGridVue} from 'ag-grid-vue'
+  import testJson from '../../../static/test/testJSON.js'
+  export default {
+    data () {
+      return {
+        colVisible: false,
+        appointlnfoVisable: false,
+        appointlnfoDelVisable: false,
+        appointlnfoForm: {
+          'choose': '',
+          'ordernumber': '',
+          'provenance': '',
+          'fromphone': '',
+          'destination': '',
+          'consignershipper': '',
+          'deliveryphone': '',
+          'receivingphone': '',
+          'receiptnumber': '',
+          'number': '',
+        //   'receiptstatus': '',
+          'receiptYaKuanstate': ''
+        },
+        rules: {
+        },
+        formLabelWidth: '150px',
+        gridOptions: {
+          context: {
+            componentParent: this
+          },
+          rowData: null,
+          columnDefs: [
+            {
+              headerName: '选择', width: 70, field: 'choose', filter: 'text', hide: false
+            },
+            {
+              headerName: '订单编号', width: 120, field: 'ordernumber', filter: 'text', hide: false
+            },
+            {
+              headerName: '始发地', width: 100, field: 'provenance', filter: 'text', hide: false
+            },
+            {
+              headerName: '始发电话', width: 120, field: 'fromphone', filter: 'text', hide: false
+            },
+            {
+              headerName: '目的地', width: 100, field: 'destination', filter: 'text', hide: false
+            },
+            {
+              headerName: '收货电话', width: 120, field: 'receivingphone', filter: 'text', hide: false
+            },
+            {
+              headerName: '回单数量', width: 100, field: 'receiptnumber', filter: 'text', hide: false
+            },
+            {
+              headerName: '件数', width: 100, field: 'number', filter: 'text', hide: false
+            },
+            {
+              headerName: '回单状态', field: 'value', width: 100, cellRendererFramework: 'operateComponent', hide: false
+            },
+            {
+              headerName: '回单押款状态', width: 150, field: 'receiptYaKuanstate', filter: 'text', hide: false
+            }
+          ]
+        }
+      }
+    },
+    components: {
+      'ag-grid-vue': AgGridVue,
+       operateComponent: {
+       template: '<span><button class="del-but">删 除</button><button class="del-but" >编 辑</button></span>',
+    //    ' <select  placeholder="' + '选择' + '" class="appointmentoption" style="width:100px"><option key="yes" label="待生成" value="yes"></option>' +
+    //                ' <option key="no" label="已生成" value="no"></option></select>',
+       }
+    },
+    methods: {
+      createRowData () {
+        this.gridOptions.rowData = testJson.transfer.list
+      },
+      onQuickFilterChanged (input) {
+        this.gridOptions.api.setQuickFilter(input)
+      },
+      changeColumnDefsBoolen () {
+        var columnlist = this.gridOptions.columnDefs
+        for (let i = 0; i < columnlist.length; i++) {
+          columnlist[i].hide = !columnlist[i].hide
+        }
+      },
+      updataColumnDefs (collist) {
+        for (let i = 0; i < collist.length; i++) {
+          this.gridOptions.columnApi.setColumnVisible(collist[i].field, collist[i].hide)
+        }
+      }
+    }
+  }
+</script>
+<style>
+.appointmenttime {
+    float: left;
+    margin: 1%;
+}
+
+.appointment {
+    padding-top: 5%;
+    width: 100%;
+    font-size: 0.6em;
+}
+
+.appointmenttimes {
+    width: 145px;
+    margin-left: 1%;
+}
+.appointmenttimeman{
+  width:100px;
+}
+
+.appointmentoption {
+    height: 30px;
+}
+</style>
+
--- static/test/testJSON.js
+++ static/test/testJSON.js
@@ -1,191 +1,197 @@
 export default {
-  personnelInfo: {
-    list: [
-      {
-        clientCompNam: 'test13',
-        nam: 'test1',
-        tel: '11111',
-        compAdr: '1111',
-        area: '1111',
-        salesmanId: '1111',
-        isTril: '1'
-      },
-      {
-        clientCompNam: 'test2',
-        nam: 'test2',
-        tel: '2222',
-        compAdr: '222',
-        area: '22',
-        salesmanId: '222',
-        isTril: '1'
-      }
-    ]
-  },
-  // 车辆信息
-  vehicleInfo: {
-    list: [
-      {
-        licePlateNum: '123456',
-        driverName: 'test',
-        tel: '12323',
-        contractID: '1234',
-        contractPrice: '1212',
-        capacity: '123',
-        tonnage: '123',
-        carType: '车辆类型',
-        pickUpArea: '区域',
-        receState: '司机状态',
-        carState: '1212',
-        carPosition: 'sdd'
-      },
-      {
-        licePlateNum: '654321',
-        driverName: 'test',
-        tel: '12323',
-        contractID: '1234',
-        contractPrice: '1212',
-        capacity: '123',
-        tonnage: '123',
-        carType: '车辆类型',
-        pickUpArea: '区域',
-        receState: '司机状态',
-        carState: '1212',
-        carPosition: 'sdd'
-      }
-    ]
-  },
-  // 外包企业
-  orderformInfo: {
-    list: [
-      {
-        liceNum: '1',
-        ordernumber: '123456',
-        orderstatus: '已发货',
-        consignor: '小王',
-        standing: '北京朝阳',
-        Shippercontactinformation: '18210182000',
-        consignee: '小龙',
-        Receivingpartycontactinformation: '13212323432',
-        descriptionofgoods: '炸药等',
-        quantity: '100吨',
-        preparedby: '小胡'
-      },
-      {
-        liceNum: '2',
-        ordernumber: '123456',
-        orderstatus: '已发货',
-        consignor: '小王',
-        standing: '北京朝阳',
-        Shippercontactinformation: '18210182000',
-        consignee: '小龙',
-        Receivingpartycontactinformation: '13212323432',
-        descriptionofgoods: '炸药等',
-        quantity: '100吨',
-        preparedby: '小胡'
-      },
-      {
-        liceNum: '3',
-        ordernumber: '123456',
-        orderstatus: '已发货',
-        consignor: '小王',
-        standing: '北京朝阳',
-        Shippercontactinformation: '18210182000',
-        consignee: '小龙',
-        Receivingpartycontactinformation: '13212323432',
-        descriptionofgoods: '炸药等',
-        quantity: '100吨',
-        preparedby: '小胡'
-      }
-    ]
-  },
-  //  外包企业信息
-  enterpriseInfo: {
-    list: [
-      {
-        companyname: '中国银行',
-        Transferstartingpoint: '北京',
-        Transferdestinatio: '南京',
-        linkman: '小李',
-        contact: '13425254345',
-        contractnumber: '35698659',
-        contractprice: '199万'
-
-      },
-      {
-        companyname: '中国银行',
-        Transferstartingpoint: '北京',
-        Transferdestinatio: '南京',
-        linkman: '小李',
-        contact: '13425254345',
-        contractnumber: '35698659',
-        contractprice: '199万'
-
-      },
-      {
-        companyname: '中国银行',
-        Transferstartingpoint: '北京',
-        Transferdestinatio: '南京',
-        linkman: '小李',
-        contact: '13425254345',
-        contractnumber: '35698659',
-        contractprice: '199万'
-
-      },
-      {
-        companyname: '中国银行',
-        Transferstartingpoint: '北京',
-        Transferdestinatio: '南京',
-        linkman: '小李',
-        contact: '13425254345',
-        contractnumber: '35698659',
-        contractprice: '199万'
-
-      }
-    ]
-
-  },
-  // 查看预约单
-  appointInfo: {
-    list: [
-      {
-        Reservationnumber: '23434532',
-        ordertime: '2017-09-02',
-        forwardingunit: '北京国贸',
-        consignershipper: '小王',
-        deliveryphone: '13329878787',
-        consignee: '南京国安',
-        consigneeman: '小龙',
-        Receivingphone: '13526598562',
-        driverreceiving: '王式安',
-        receivingstate: '已接货',
-        Cancelreservations: '否'
-      },
-      {
-        Reservationnumber: '23434532',
-        ordertime: '2017-09-02',
-        forwardingunit: '北京国贸',
-        consignershipper: '小王',
-        deliveryphone: '13329878787',
-        consignee: '南京国安',
-        consigneeman: '小龙',
-        Receivingphone: '13526598562',
-        driverreceiving: '王式安',
-        receivingstate: '已接货',
-        Cancelreservations: '否'
-      },
-      {
-        Reservationnumber: '23434532',
-        ordertime: '2017-09-02',
-        forwardingunit: '北京国贸',
-        consignershipper: '小王',
-        deliveryphone: '13329878787',
-        consignee: '南京国安',
-        consigneeman: '小龙',
-        Receivingphone: '13526598562',
-        driverreceiving: '王式安',
-        receivingstate: '已接货',
-        Cancelreservations: '否'
-      }
-    ]
-  }
-}
+    personnelInfo: {
+        list: [{
+                clientCompNam: "test13",
+                nam: "test1",
+                tel: "11111",
+                compAdr: "1111",
+                area: "1111",
+                salesmanId: "1111",
+                isTril: "1"
+            },
+            {
+                clientCompNam: "test2",
+                nam: "test2",
+                tel: "2222",
+                compAdr: "222",
+                area: "22",
+                salesmanId: "222",
+                isTril: "1"
+            }
+        ]
+    },
+    // 车辆信息
+    vehicleInfo: {
+        list: [{
+                licePlateNum: "123456",
+                driverName: "test",
+                tel: "12323",
+                contractID: "1234",
+                contractPrice: "1212",
+                capacity: "123",
+                tonnage: "123",
+                carType: "车辆类型",
+                pickUpArea: "区域",
+                receState: "司机状态",
+                carState: "1212",
+                carPosition: "sdd"
+            },
+            {
+                licePlateNum: "654321",
+                driverName: "test",
+                tel: "12323",
+                contractID: "1234",
+                contractPrice: "1212",
+                capacity: "123",
+                tonnage: "123",
+                carType: "车辆类型",
+                pickUpArea: "区域",
+                receState: "司机状态",
+                carState: "1212",
+                carPosition: "sdd"
+            }
+        ]
+    },
+    // 外包企业
+    orderformInfo: {
+        list: [{
+                liceNum: "1",
+                ordernumber: "123456",
+                orderstatus: "已发货",
+                consignor: "小王",
+                standing: "北京朝阳",
+                Shippercontactinformation: "18210182000",
+                consignee: "小龙",
+                Receivingpartycontactinformation: "13212323432",
+                descriptionofgoods: "炸药等",
+                quantity: "100吨",
+                preparedby: "小胡"
+            },
+            {
+                liceNum: "2",
+                ordernumber: "123456",
+                orderstatus: "已发货",
+                consignor: "小王",
+                standing: "北京朝阳",
+                Shippercontactinformation: "18210182000",
+                consignee: "小龙",
+                Receivingpartycontactinformation: "13212323432",
+                descriptionofgoods: "炸药等",
+                quantity: "100吨",
+                preparedby: "小胡"
+            },
+            {
+                liceNum: "3",
+                ordernumber: "123456",
+                orderstatus: "已发货",
+                consignor: "小王",
+                standing: "北京朝阳",
+                Shippercontactinformation: "18210182000",
+                consignee: "小龙",
+                Receivingpartycontactinformation: "13212323432",
+                descriptionofgoods: "炸药等",
+                quantity: "100吨",
+                preparedby: "小胡"
+            }
+        ]
+    },
+    //  外包企业信息
+    enterpriseInfo: {
+        list: [{
+                companyname: "中国银行",
+                Transferstartingpoint: "北京",
+                Transferdestinatio: "南京",
+                linkman: "小李",
+                contact: "13425254345",
+                contractnumber: "35698659",
+                contractprice: "199万"
+            },
+            {
+                companyname: "中国银行",
+                Transferstartingpoint: "北京",
+                Transferdestinatio: "南京",
+                linkman: "小李",
+                contact: "13425254345",
+                contractnumber: "35698659",
+                contractprice: "199万"
+            },
+            {
+                companyname: "中国银行",
+                Transferstartingpoint: "北京",
+                Transferdestinatio: "南京",
+                linkman: "小李",
+                contact: "13425254345",
+                contractnumber: "35698659",
+                contractprice: "199万"
+            },
+            {
+                companyname: "中国银行",
+                Transferstartingpoint: "北京",
+                Transferdestinatio: "南京",
+                linkman: "小李",
+                contact: "13425254345",
+                contractnumber: "35698659",
+                contractprice: "199万"
+            }
+        ]
+    },
+    // 查看预约单
+    appointInfo: {
+        list: [{
+                Reservationnumber: "23434532",
+                ordertime: "2017-09-02",
+                forwardingunit: "北京国贸",
+                consignershipper: "小王",
+                deliveryphone: "13329878787",
+                consignee: "南京国安",
+                consigneeman: "小龙",
+                Receivingphone: "13526598562",
+                driverreceiving: "王式安",
+                receivingstate: "已接货",
+                Cancelreservations: "否"
+            },
+            {
+                Reservationnumber: "23434532",
+                ordertime: "2017-09-02",
+                forwardingunit: "北京国贸",
+                consignershipper: "小王",
+                deliveryphone: "13329878787",
+                consignee: "南京国安",
+                consigneeman: "小龙",
+                Receivingphone: "13526598562",
+                driverreceiving: "王式安",
+                receivingstate: "已接货",
+                Cancelreservations: "否"
+            },
+            {
+                Reservationnumber: "23434532",
+                ordertime: "2017-09-02",
+                forwardingunit: "北京国贸",
+                consignershipper: "小王",
+                deliveryphone: "13329878787",
+                consignee: "南京国安",
+                consigneeman: "小龙",
+                Receivingphone: "13526598562",
+                driverreceiving: "王式安",
+                receivingstate: "已接货",
+                Cancelreservations: "否"
+            }
+        ]
+    },
+    transfer: {
+        list: [{
+            choose: '2',
+            ordernumber: '123432',
+            provenance: "beijing",
+            fromphone: "123879878987",
+            destination: "shanghai",
+            consignershipper: "13223434565",
+            deliveryphone: "南京",
+            receivingphone: "132456544554",
+            receiptnumber: "89箱",
+            number: "90",
+            // receiptstatus: "已付款",
+            receiptYaKuanstate: "已回单"
+        }]
+    }
+};
