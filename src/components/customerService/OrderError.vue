4d01e977aa424323e152d7368a561927f8fc8fce
lilili <939033853@qq.com>
2017-08-30 18:13:08 +0800

    增加客服相关页面，更改侧栏信息

---
+++ src/components/customerService/OrderError.vue
@@ -0,0 +1,238 @@
+<template>
+  <div>
+    <div>
+      <h2 style="text-align:center">订单异常处理</h2>
+      <p style="margin-top:1%">
+        <div style="float: right;margin-right:16%">
+          <el-input type="text" placeholder="请输入搜索内容" @input="onQuickFilterChanged"></el-input>
+        </div>
+        <div style="margin-left:12%">
+          <el-button @click="setAddVisable">添加</el-button>
+          <el-button @click="setting">设置</el-button>
+        </div>
+      </p>
+    </div>
+    <div style="clear: both;">
+    </div>
+    <div style="margin-top:2%">
+      <ag-grid-vue style="width: 72%;margin-left:12%;height: 350px" class="ag-blue" 
+        :gridOptions="gridOptions" 
+        :suppressMovableColumns="true" 
+        :enableColResize="true" 
+        :enableSorting="true" 
+        :enableFilter="true" 
+        :groupHeaders="true" 
+        :suppressCellSelection="true"
+        :rowHeight=40
+        :headerHeight=30
+        :pagination="true"
+        :paginationPageSize="10"
+        :suppressPaginationPanel="true"
+        :filterChanged="gridfilterChange">
+      </ag-grid-vue>
+    </div>
+
+    <!--分页-->
+    <div style="text-align: center;margin-top:2%">
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrnetChange"
+        :current-page="currentPage"
+        :page-sizes="[10,20,50,100,200]"
+        :page-size="pageSize"
+        layout="total,sizes,prev,pager,next"
+        :total="rowCount">
+      </el-pagination>
+    </div>
+
+    <!-- 编辑/添加异常信息弹窗 -->
+    <el-dialog title="更改订单异常信息:" :visible.sync="addEditVisable"  >
+      <el-form :model="orderErrorForm" :rules="rules" ref="orderErrorForm">
+        <el-form-item label="订单ID：" :label-width="formLabelWidth">
+          <el-input v-model="orderErrorForm.orderId" style='width:80%'></el-input>
+        </el-form-item>
+        <el-form-item label="订单当前位置：" :label-width="formLabelWidth">
+          <el-input v-model="orderErrorForm.currPosition" style='width:80%'></el-input>
+        </el-form-item>
+        <el-form-item label="订单物流状态：" :label-width="formLabelWidth">
+          <el-input v-model="orderErrorForm.orderLogiState" style='width:80%'></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="addEditVisable = false">取 消</el-button>
+        <el-button type="primary" @click="addEdit">确 定</el-button>
+      </div>
+    </el-dialog>
+     <!-- 删除弹窗 -->
+    <el-dialog title="" :visible.sync="delVisable" size="tiny">
+      <h2 style="padding:30px">确认删除吗？</h2>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="delVisable = false">取 消</el-button>
+        <el-button @click="del">确 定</el-button>
+      </div>
+    </el-dialog>
+
+    <!-- 列表显示弹窗 -->
+    <el-dialog title="选择要显示的列表:" :visible.sync="colVisible" size="tiny" :closeOnClickModal="false">
+      <template v-for="(collist,i) in gridOptions.columnDefs">
+        <div>
+          <el-checkbox v-model="collist.hide" @change="updataColumnDefs(gridOptions.columnDefs)">
+            {{collist.headerName}}
+          </el-checkbox>
+        </div>
+      </template>
+      <div slot="footer" class="dialog-footer">
+        <el-button type="primary" @click="colVisible = false">确 定</el-button>
+      </div>
+    </el-dialog>
+
+    <!-- 增加/编辑成功弹窗 -->
+    <el-dialog title="" :visible.sync="successVisable" size="" tiny>
+      <h2 style="padding:30px">异动信息提交成功！</h2>
+    </el-dialog>
+
+  </div>
+</template>
+<script>
+import { AgGridVue } from 'ag-grid-vue'
+export default {
+  created () {
+    for (var i = 0; i < 50; i++) {
+      this.orderErrorList.push({
+        'orderId': '订单ID' + i,
+        'orderLogiState': '物流状态' + i,
+        'currPosition': '订单当前位置' + i
+      })
+    }
+  },
+  data () {
+    return {
+      orderErrorList: [],
+      colVisible: false,
+      addEditVisable: false,
+      delVisable: false,
+      successVisable: false,
+      orderErrorForm: {
+        'orderId': '',
+        'orderLogiState': '',
+        'currPosition': ''
+      },
+      rules: {
+      },
+      rowCount: 0,
+      pageSize: 10,
+      formLabelWidth: '20%',
+      gridOptions: {
+        context: {
+          componentParent: this
+        },
+        rowData: null,
+        columnDefs: [
+          {
+            headerName: '订单ID', width: 200, field: 'orderId', filter: 'text', hide: false
+          },
+          {
+            headerName: '物流状态更改', width: 200, field: 'orderLogiState', filter: 'text', hide: false
+          },
+          {
+            headerName: '订单位置更改', width: 200, field: 'currPosition', filter: 'text', hide: false
+          },
+          {
+            headerName: '操作', field: 'value', width: 240, cellRendererFramework: 'operateComponent', hide: false
+          }
+        ]
+      }
+    }
+  },
+  components: {
+    'ag-grid-vue': AgGridVue,
+    operateComponent: {
+      template: '<span><button class="del-but" @click="setDelVisable">删 除</button><button class="del-but" @click="setEditVisable">编 辑</button></span>',
+      methods: {
+        setDelVisable () {
+          this.params.context.componentParent.delVisable = true
+        },
+        setEditVisable () {
+          let self = this.params.context.componentParent
+          /* var vehicleform = this.params.context.componentParent.vehicleForm
+          vehicleform.licePlateNum = vehicleList[this.params.node.rowIndex].licePlateNum */
+          self.addEditVisable = true
+          self.orderErrorForm = this.params.data
+        }
+      }
+    }
+  },
+  methods: {
+    createRowData () {
+      this.gridOptions.rowData = this.orderErrorList
+    },
+    onQuickFilterChanged (input) {
+      this.gridOptions.api.setQuickFilter(input)
+    },
+    changeColumnDefsBoolen () {
+      var columnlist = this.gridOptions.columnDefs
+      for (let i = 0; i < columnlist.length; i++) {
+        columnlist[i].hide = !columnlist[i].hide
+      }
+    },
+    setting () {
+      this.colVisible = true
+    },
+    updataColumnDefs (collist) {
+      for (let i = 0; i < collist.length; i++) {
+        this.gridOptions.columnApi.setColumnVisible(collist[i].field, collist[i].hide)
+      }
+    },
+    // 设置增加弹窗
+    setAddVisable () {
+      this.addEditVisable = true
+      this.orderErrorForm.orderId = ''
+      this.orderErrorForm.orderLogiState = ''
+      this.orderErrorForm.currPosition = ''
+    },
+    // 编辑/增加异动信息
+    addEdit () {
+      setTimeout(() => { this.addEditVisable = false }, 600)
+    },
+    // 删除异动信息
+    del () {
+      this.delVisable = false
+    },
+    handleSizeChange (val) {
+      this.gridOptions.api.paginationSetPageSize(Number(val))
+    },
+    handleCurrnetChange (val) {
+      this.gridOptions.api.paginationGoToPage(val - 1)
+    },
+    gridfilterChange () {
+      this.calculateGrid()
+    },
+    // 设置分页组件数据总数
+    calculateGrid () {
+      this.gridOptions.api.paginationSetPageSize(Number(this.pageSize))
+      this.rowCount = this.gridOptions.api.getModel().getRowCount()
+    }
+  },
+  beforeMount () {
+    this.createRowData()
+  },
+  mounted () {
+    this.changeColumnDefsBoolen()
+    this.calculateGrid()
+  }
+}
+</script>
+<style>
+.del-but {
+  cursor: pointer;
+  float: right;
+  margin-right: 16%;
+  margin-top: 2%;
+  border-radius: 4px;
+  background: #fff;
+  border: 1px solid rgb(191, 217, 216);
+  color: rgb(31, 61, 60);
+  padding: 5px 10px;
+  font-size: 10px
+}
+</style>
--- src/components/customerService/OrderInfo.vue
+++ src/components/customerService/OrderInfo.vue
@@ -1,3 +1,193 @@
 <template>
-    <div>查看库存</div>
+  <div>
+    <div>
+      <h2 style="text-align:center">订单列表</h2>
+      <p style="margin-top:1%">
+        <div style="float: right">
+          <el-input type="text" placeholder="请输入搜索内容" @input="onQuickFilterChanged"></el-input>
+        </div>
+        <div>
+          <el-button @click="toOrderAdd">添加</el-button>
+          <el-button @click="setting">设置</el-button>
+        </div>
+      </p>
+    </div>
+    <div style="clear: both;">
+    </div>
+    <div style="margin-top:2%">
+      <ag-grid-vue style="width: 100%;height: 350px" class="ag-blue" 
+        :gridOptions="gridOptions" 
+        :suppressMovableColumns="true" 
+        :enableColResize="true" 
+        :enableSorting="true" 
+        :enableFilter="true" 
+        :groupHeaders="true" 
+        :suppressCellSelection="true"
+        :rowHeight=40 
+        :headerHeight=30
+        :pagination="true"
+        :paginationPageSize="10"
+        :suppressPaginationPanel="true"
+        :filterChanged="gridfilterChange"
+        :rowDoubleClicked="detailDoubleClick">
+      </ag-grid-vue>
+    </div>
+
+    <!--分页-->
+    <div style="text-align: center;margin-top:2%">
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrnetChange"
+        :current-page="currentPage"
+        :page-sizes="[10,20,50,100,200]"
+        :page-size="pageSize"
+        layout="total,sizes,prev,pager,next"
+        :total="rowCount">
+      </el-pagination>
+    </div>
+
+    <!-- 列表显示弹窗 -->
+    <el-dialog title="选择要显示的列表:" :visible.sync="colVisible" size="tiny" :closeOnClickModal="false">
+      <template v-for="(collist,i) in gridOptions.columnDefs">
+        <div>
+          <el-checkbox v-model="collist.hide" @change="updataColumnDefs(gridOptions.columnDefs)">
+            {{collist.headerName}}
+          </el-checkbox>
+        </div>
+      </template>
+      <div slot="footer" class="dialog-footer">
+        <el-button type="primary" @click="colVisible = false">确 定</el-button>
+      </div>
+    </el-dialog>
+
+    <!-- 订单详情弹窗 -->
+    <el-dialog title="订单详情:" :visible.sync="detailVisible" size="small" :closeOnClickModal="false">
+      <order-details :orderId="orderList.orderId"></order-details>
+    </el-dialog>
+  </div>
 </template>
+<script>
+import { AgGridVue } from 'ag-grid-vue'
+import OrderDetails from '../financialAdministrator/ShowOrderDetails.vue'
+export default {
+  created () {
+    for (var i = 0; i < 50; i++) {
+      this.orderList.push({
+        'orderId': 'asasas' + i,
+        'shipComp': '发货单位' + i,
+        'shipNam': '发货人' + i,
+        'receComp': '收货单位' + i,
+        'receNam': '收货人' + i,
+        'goodsNam': '货物名称' + i,
+        'goodsNums': i,
+        'goodsVolume': '货物体积' + i,
+        'goodsWeight': '货物重量' + i
+      })
+    }
+  },
+  data () {
+    return {
+      orderList: [],
+      colVisible: false,
+      detailVisible: false,
+      currentPage: 1,
+      rules: {
+      },
+      rowCount: 0,
+      pageSize: 10,
+      formLabelWidth: '20%',
+      gridOptions: {
+        context: {
+          componentParent: this
+        },
+        rowData: null,
+        columnDefs: [
+          {
+            headerName: '订单ID', width: 150, field: 'orderId', filter: 'text', hide: false
+          },
+          {
+            headerName: '发货单位', width: 150, field: 'shipComp', filter: 'text', hide: false
+          },
+          {
+            headerName: '发货人', width: 150, field: 'shipNam', filter: 'text', hide: false
+          },
+          {
+            headerName: '收货单位', width: 150, field: 'receComp', filter: 'text', hide: false
+          },
+          {
+            headerName: '收货人', width: 150, field: 'receNam', filter: 'text', hide: false
+          },
+          {
+            headerName: '货物名称', field: 'goodsNam', width: 150, filter: 'text', hide: false
+          },
+          {
+            headerName: '件数', field: 'goodsNums', width: 150, filter: 'text', hide: false
+          },
+          {
+            headerName: '体积', field: 'goodsVolume', width: 150, filter: 'text', hide: false
+          },
+          {
+            headerName: '重量', field: 'goodsWeight', width: 150, filter: 'text', hide: false
+          }
+        ]
+      }
+    }
+  },
+  components: {
+    OrderDetails,
+    'ag-grid-vue': AgGridVue
+  },
+  methods: {
+    // 订单详情弹框
+    detailDoubleClick (event) {
+      this.orderList.orderId = event.data.orderId
+      this.detailVisible = true
+    },
+    createRowData () {
+      this.gridOptions.rowData = this.orderList
+    },
+    onQuickFilterChanged (input) {
+      this.gridOptions.api.setQuickFilter(input)
+    },
+    changeColumnDefsBoolen () {
+      var columnlist = this.gridOptions.columnDefs
+      for (let i = 0; i < columnlist.length; i++) {
+        columnlist[i].hide = !columnlist[i].hide
+      }
+    },
+    setting () {
+      this.colVisible = true
+    },
+    updataColumnDefs (collist) {
+      for (let i = 0; i < collist.length; i++) {
+        this.gridOptions.columnApi.setColumnVisible(collist[i].field, collist[i].hide)
+      }
+    },
+    // 跳转到网点开单页面
+    toOrderAdd () {
+      this.$router.push('/orderAdd')
+    },
+    handleSizeChange (val) {
+      this.gridOptions.api.paginationSetPageSize(Number(val))
+    },
+    handleCurrnetChange (val) {
+      this.gridOptions.api.paginationGoToPage(val - 1)
+    },
+    gridfilterChange () {
+      this.calculateGrid()
+    },
+    // 设置分页组件数据总数
+    calculateGrid () {
+      this.gridOptions.api.paginationSetPageSize(Number(this.pageSize))
+      this.rowCount = this.gridOptions.api.getModel().getRowCount()
+    }
+  },
+  beforeMount () {
+    this.createRowData()
+  },
+  mounted () {
+    this.changeColumnDefsBoolen()
+    this.calculateGrid()
+  }
+}
+</script>
