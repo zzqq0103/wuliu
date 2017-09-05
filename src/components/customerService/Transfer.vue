bd9e2f4d3b9dbbde302d3c4a4592e677627816a5
yty <yantingyong86@163.com>
2017-09-05 16:32:28 +0800

    修改。。

--- src/components/customerService/Transfer.vue
+++ src/components/customerService/Transfer.vue
@@ -4,9 +4,9 @@
             <h2 style="text-align:center">回单管理</h2>
             <p style="margin-top:1%;float:left" class="appointment" :model="transfer">
                 <el-h5 class="appointmenttime">开单时间</el-h5>
-                <el-date-picker type="date" class="appointmenttimes" placeholder="开始时间" v-model="transfer.startTime"></el-date-picker>
+                <el-date-picker type="datetime" class="appointmenttimes" placeholder="开始时间" v-model="transfer.startTime"></el-date-picker>
                 <el-h5 class="appointmenttimes">到</el-h5>
-                <el-date-picker type="date" class="appointmenttimes" placeholder="结束时间" v-model="transfer.endTime"></el-date-picker>
+                <el-date-picker type="datetime" class="appointmenttimes" placeholder="结束时间" v-model="transfer.endTime"></el-date-picker>
                 <el-h5 class="appointmenttimes">预约单号：</el-h5>
                 <el-input class="appointmenttimeman"></el-input>
                 <el-h5 class="appointmenttimes">回单状态：</el-h5>
@@ -34,8 +34,8 @@
         </div>
         <div>
           <p style="margin-top:1%;float:right;margin-right:5%;width:50%">
-                <el-button style="margin-right:6%">确认更新</el-button>
-                <el-button>全选</el-button>
+                <el-button style="margin-right:6%">确 认 更 新</el-button>
+                <!--<el-button>全选</el-button>-->
           </p>
         </div>
     </div>
@@ -63,7 +63,7 @@
           rowData: null,
           columnDefs: [
             {
-              headerName: '选择', width: 70, field: 'choose', filter: 'text', hide: false
+              headerName: '序号', width: 70, field: 'choose', filter: 'text', hide: false
             },
             {
               headerName: '订单编号', width: 120, field: 'ordernumber', filter: 'text', hide: false
@@ -99,7 +99,7 @@
     components: {
       'ag-grid-vue': AgGridVue,
       operateComponent: {
-        template: '<el-select v-model="appointlnfoForm.Selectsites"  placeholder="选择" class="appointmentoption col-1" style="width:130px"> <el-option key="yes" label=" 回单已签 " value="yes"></el-option><el-option key="yes" label=" 回单在途 " value="no"></el-option></el-select>',
+        template: '<el-select v-model="appointlnfoForm.Selectsites"  placeholder="选择" class="appointmentoption col-1" style="width:130px"> <el-option key="yes" label=" 回单已签 " value="yes"></el-option><el-option key="yes" label=" 回单在途 " value=""></el-option></el-option><el-option key="yes" label=" 回单已返 " value="no"></el-option></el-select>',
         // '<select placeholder="选择" class="appointmentoption" style="width:150px;heght:60px"><option key="yes" label="回单已签" value="yes"></option><option key="no" label="回单在途" value="no"></option><option key="no" label="回单已反" value="no"></option> </select>'
         data: function () {
           return {
@@ -159,6 +159,9 @@
 
 .appointmentoption {
     height: 30px;
+}
+.ag-blue .ag-cell-focus {
+  border: 0px solid #217346;
 }
 </style>
 
