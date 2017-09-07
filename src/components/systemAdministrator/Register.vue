b4e794f64e8592fdd9d2f386c1b3ecc4578a3eec
SPD <peidong.sun@foxmail.com>
2017-09-07 13:31:04 +0800

    Merge branch 'dev' of https://git.oschina.net/FangFengDewall/WuLiuQianDuanXiangMu into spd
    # Conflicts:
    #	src/components/dispatch/deliveredInfo.vue
    
    合并dev

--- src/components/systemAdministrator/Register.vue
+++ src/components/systemAdministrator/Register.vue
@@ -1,119 +1,99 @@
 <template>
-  <div style="border: 2px solid #000000; width: 90%; height: 100%; text-align: center">
-    <div style="margin-top: 5%">
-      <h2 style="text-align:center">账 号 注 册</h2>
-    </div>
-
-    <el-col :span="7" :offset="9">
-      <!--下拉选择：角色-->
-      <div style="margin-top: 15%">
-        <el-form>
-          <el-form-item label="角色类型">
-            <el-select v-model="selectvalue" :placeholder="queryItemOptions[0].label" style="width:195px">
-              <el-option v-for="item in queryItemOptions" :key="item.value" :label="item.label" :value="item.value">
-              </el-option>
-            </el-select>
-          </el-form-item>
-        </el-form>
-      </div>
-      <!--输入：手机号-->
-      <div style="margin-top: 9%">
-        <el-form>
-          <el-form-item label="手机号">
-            <el-input type="text" placeholder="输入手机号" style="width: 210px"></el-input>
-          </el-form-item>
-        </el-form>
-      </div>
-      <!--输入：密码-->
-      <div>
-        <el-form>
-          <el-form-item label="密码">
-            <el-input type="text" placeholder="密码不少于六位" style="width: 223px"></el-input>
-          </el-form-item>
-        </el-form>
-      </div>
-      <!--输入：确认密码-->
-
-      <div>
-        <el-form>
-          <el-form-item label="确认密码">
-            <el-input type="text" placeholder="再次输入密码" style="width: 195px"></el-input>
-          </el-form-item>
-        </el-form>
-      </div>
-      <!--按钮：立即注册-->
-      <div style="float: right">
-        <el-button type="primary" @click="confirmRegister">立即注册</el-button>
-      </div>
-      <!--按钮：取消-->
-      <div style="float: left">
-        <el-button @click="confirmCancel">取消</el-button>
-      </div>
-    </el-col>
-  </div>
+  <el-col span="7" offset="7" style="margin-top: 10%">
+    <el-form :model="ruleForm2" :rules="rules2" ref="ruleForm2" label-width="100px" class="demo-ruleForm">
+      <el-form-item label="手机号" prop="phone">
+        <el-input v-model.number="ruleForm2.phone"></el-input>
+      </el-form-item>
+      <el-form-item label="密码" prop="pass">
+        <el-input type="password" v-model="ruleForm2.pass" auto-complete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="确认密码" prop="checkPass">
+        <el-input type="password" v-model="ruleForm2.checkPass" auto-complete="off"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="submitForm('ruleForm2')" class="register">注 册</el-button>
+      </el-form-item>
+    </el-form>
+  </el-col>
 </template>
 
-
 <script>
-  export default{
-    components: {},
-    methods: {
-      confirmRegister () {
-      },
-      confirmCancel () {
+  export default {
+    data () {
+      var checkPhone = (rule, value, callback) => {
+        if (!value) {
+          return callback(new Error('手机号不能为空'))
+        }
+        setTimeout(() => {
+          if (!Number.isInteger(value)) {
+            callback(new Error('请输入数字值'))
+          } else {
+            if (value < 18) {
+              callback(new Error('必须年满18岁'))
+            } else {
+              callback()
+            }
+          }
+        }, 1000)
+      }
+      var validatePass = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('请输入密码'))
+        } else {
+          if (this.ruleForm2.checkPass !== '') {
+            this.$refs.ruleForm2.validateField('checkPass')
+          }
+          callback()
+        }
+      }
+      var validatePass2 = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('请再次输入密码'))
+        } else if (value !== this.ruleForm2.pass) {
+          callback(new Error('两次输入密码不一致!'))
+        } else {
+          callback()
+        }
+      }
+      return {
+        ruleForm2: {
+          pass: '',
+          checkPass: '',
+          phone: ''
+        },
+        rules2: {
+          pass: [
+            {validator: validatePass, trigger: 'blur'}
+          ],
+          checkPass: [
+            {validator: validatePass2, trigger: 'blur'}
+          ],
+          phone: [
+            {validator: checkPhone, trigger: 'blur'}
+          ]
+        }
       }
     },
-    data () {
-      return {
-        selectvalue: 1,
-        queryItemOptions: [{
-          value: 1,
-          label: '调度管理员'
-        }, {
-          value: 2,
-          label: '客服人员'
-        }, {
-          value: 3,
-          label: '外包人员'
-        }, {
-          value: 4,
-          label: '财务核销人员'
-        }, {
-          value: 5,
-          label: '财务审核人员'
-        }, {
-          value: 6,
-          label: '业务员'
-        }]
+    methods: {
+      submitForm (formName) {
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+            alert('submit!')
+          } else {
+            console.log('error submit!!')
+            return false
+          }
+        })
       }
     }
   }
 </script>
 
 <style>
-  .el-col {
-    border-radius: 4px;
-  }
-
-  .bg-purple-dark {
-    background: #99a9bf;
-  }
-
-  .bg-purple {
-    background: #00ff00;
-  }
-
-  .bg-purple-light {
-    background: #e5e9f2;
-  }
-
-  .grid-content {
-    border-radius: 4px;
-    min-height: 36px;
-  }
-
-  .row-bg {
-    padding: 10px 0;
-    background-color: #f9fafc;
+  .register {
+    margin-top: 3%;
+    font-size: large;
+    height: 40px;
+    width: 215px;
   }
 </style>
