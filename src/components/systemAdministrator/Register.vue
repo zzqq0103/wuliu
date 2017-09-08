82e00aa4af5c3e3ea4fcc5962852bffcf610e210
SPD <peidong.sun@foxmail.com>
2017-09-08 15:15:35 +0800

    Merge branch 'dev' of https://git.oschina.net/FangFengDewall/WuLiuQianDuanXiangMu into spd
    # Conflicts:
    #	src/components/dispatch/deliveredInfo.vue
    
    合并dev

--- src/components/systemAdministrator/Register.vue
+++ src/components/systemAdministrator/Register.vue
@@ -7,6 +7,12 @@
           </el-option>
         </el-select>
       </el-form-item>
+      <el-form-item label="站点/网点">
+        <el-select v-model="ruleForm2.station" :placeholder="ruleForm2.queryStationOptions[0].label" style="width: 300px">
+          <el-option span="14" v-for="item in ruleForm2.queryStationOptions" :key="item.value" :label="item.label" :value="item.value">
+          </el-option>
+        </el-select>
+      </el-form-item>
       <el-form-item label="姓 名" prop="name">
         <el-input v-model="ruleForm2.name" placeholder="输入姓名" style="width: 300px"></el-input>
       </el-form-item>
@@ -18,9 +24,6 @@
       </el-form-item>
       <el-form-item label="确认密码" prop="checkPass">
         <el-input v-model="ruleForm2.checkPass" auto-complete="off" style="width: 300px"></el-input>
-      </el-form-item>
-      <el-form-item label="站点 / 网点" prop="station">
-        <el-input v-model="ruleForm2.station" placeholder="输入所属 站点/网点" style="width: 300px"></el-input>
       </el-form-item>
       <el-form-item>
         <el-button type="primary" @click="submitForm('ruleForm2')" class="register">注 册</el-button>
@@ -73,15 +76,10 @@
         }
         callback()
       }
-      var checkStation = (rule, value, callback) => {
-        if (!value) {
-          return callback(new Error('站点 / 网点 不能为空'))
-        }
-        callback()
-      }
       return {
         ruleForm2: {
           selectvalue: 1,
+          station: 1,
           queryItemOptions: [{
             value: 1,
             label: '调度管理员'
@@ -101,8 +99,14 @@
             value: 6,
             label: '业务员'
           }],
+          queryStationOptions: [{
+            value: 1,
+            label: '北京'
+          }, {
+            value: 2,
+            label: '南京'
+          }],
           name: '',
-          station: '',
           pass: '666666',
           checkPass: '666666',
           phone: ''
@@ -119,9 +123,6 @@
           ],
           name: [
             {validator: checkName, trigger: 'blur'}
-          ],
-          station: [
-            {validator: checkStation, trigger: 'blur'}
           ]
         }
       }
